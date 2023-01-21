49b5b90851faf9f428ea93fb03ae62b68b2210ca
Zin Min Htet <73432858+Zakerxa@users.noreply.github.com>
2023-01-22 00:19:02 +0630

    Update index.php

--- 2d/history/index.php
+++ 2d/history/index.php
@@ -3,7 +3,7 @@
 require join(DIRECTORY_SEPARATOR, array(__DIR__, "../..","app/class", "2dClass.php"));
 require join(DIRECTORY_SEPARATOR, array(__DIR__, "../..","app/class", "requestLimit.php"));
 
-$allowed_hosts = array("zplus2d.com", "burma2d3d.com", "localhost", "localhost:8080", "localhost:8000", "127.0.0.1");
+$allowed_hosts = array("localhost", "localhost:8080", "localhost:8000", "127.0.0.1");
 
 if (!isset($_SERVER['HTTP_HOST']) || !in_array($_SERVER['HTTP_HOST'], $allowed_hosts)) {
     Server::ApiRateLimit(60, 3, Myanmar2D::ShowHistory((20)),"3 requests per minute.");
